f7e68bdefeae7f36e5b785c5ad601ca02a1179a5
Julien Topçu <julien.topcu@gmail.com>
2019-01-20 19:55:57 +0100

    enriching the skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BestsellersComponent } from './bestsellers/bestsellers.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BestsellersComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule
